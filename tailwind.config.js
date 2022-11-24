be3d38c2dd44bf17772c93ed4196e5b3345c2300
Mandor99 <mahmoudmandor65@gmail.com>
2022-11-24 19:58:00 +0200

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/*.{html,js}"],
+  theme: {
+    extend: {},
+    fontFamily: ["'Lato', sans-serif"]
+  },
+  plugins: [],
+}
